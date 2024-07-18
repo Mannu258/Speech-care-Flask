528e567678c7c2cd3b24c3a4d3682f92045bedef
Your Name <you@example.com>
2024-07-18 18:37:55 +0530

    some error occered

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3.db'  # Replace with your database URL
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "sqlite:///sqlite3.db"  # Replace with your database URL
+)
 db = SQLAlchemy(app)
+
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = "jeemannu90@gmail.com"
+app.config["MAIL_PASSWORD"] = "icbh jolb cnuv dnbq"
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+
+
+mail = Mail(app)
+mail.init_app(app)
 
 with app.app_context():
     db.create_all()
 
-class Details(db.Model):
+
+class Detail_Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(60), nullable=False)
     Email = db.Column(db.String(120), unique=True, nullable=False)
@@ -20,7 +36,8 @@ class Details(db.Model):
     def __repr__(self):
         return f"<Details {self.Name} {self.Date}>"
 
-class Email(db.Model):
+
+class Email_Model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Email = db.Column(db.String(120), unique=True, nullable=False)
     Date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -29,10 +46,34 @@ class Email(db.Model):
         return f"<Email {self.Email} {self.Date}>"
 
 
-@app.route('/')
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        try:
+            name = request.form["name"]
+            email = request.form["email"]
+            mobile = request.form["mobile"]
+            msg = request.form["msg"]
+            print("All details is running")
+            details = Detail_Model(Name=name, Email=email, Mobile=mobile, Message=msg)
+            db.session.add(details)
+            db.session.commit()
+            try:
+                from Auto_mail import send_mail
+                send_mail(name, email, mobile, msg)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+            return render_template("Thank-You.html")
+        except:
+            email = request.form["email"]
+            print("Only single mail is running")
+            from Auto_mail import single_mail
+            single_mail(email)
+            e = Email_Model(Email=email)
+            db.session.add(e)
+            db.session.commit()        
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
